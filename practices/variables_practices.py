db8be600ec27af1fcf03e15ac9917d153e797efe
ZaleyBear <alice.gibbons@ucas-edu.net>
2026-01-22 11:28:13 -0700

    commit

---
+++ practices/variables_practices.py
@@ -0,0 +1,13 @@
+# AZG, Variables Practice
+
+name="Azalea"
+food="Biscuits and Gravy"
+number=7
+number2=789
+color="light pink"
+school="UCAS"
+year="2026"
+eyecolor="green" 
+age="14"
+subject="math"
+print(name, food, number, number2, color, school, year, eyecolor, age, subject)
