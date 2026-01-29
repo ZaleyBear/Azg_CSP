ccaec079a65d4d0a7fc64e174589ed27a1b96d94
ZaleyBear <alice.gibbons@ucas-edu.net>
2026-01-29 11:28:50 -0700

    Silly

---
+++ practices/silly sentences.py
@@ -0,0 +1,7 @@
+#AZG Python 
+verb = input("Tell me a verb ending in ing: ")
+place=input("tell me a place: ")
+object=input("tell me an object: ")
+emotion=input("tell me an emotion: ")
+color=input("tell me a color: ")
+print("i was", verb, "in", place, "when i tripped on a/an", object,"...", "I was so", emotion,"that I dyed my hair", color)
