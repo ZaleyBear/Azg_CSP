a77a3367fbcc1bfa83130744fa3eed7a429edf59
ZaleyBear <alice.gibbons@ucas-edu.net>
2026-02-24 11:59:39 -0700

    larose setup

---
+++ c/notes/strings.c
@@ -0,0 +1,26 @@
+// Azg strings notes
+#include <studio.h>
+#include <strings.h>
+int main(){
+   char subject[] = "Computer Science Principles"
+   char book[50];
+   char title "Ms.LaRose";
+   int num;
+   printf("%s", subject)
+   num = 10
+   printf("write the title of a book") 
+   fgets(book, sizeof(book), stdin)
+   primtf("\nThe book is %s. 
+   that is a cool book\n", book);
+
+   char name[]= "Vienna";
+   last[] = "LaRode";
+   strcat(name, " ");
+   strcat(name, last);
+
+char full_name[50]
+strcat
+   printf("%s\n", full name);// ;
+
+    return ;0}
+
