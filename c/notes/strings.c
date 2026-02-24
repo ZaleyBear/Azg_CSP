a4a4fd94b1fe2f0a1e9b52ae294895b708dcbc3d
ZaleyBear <alice.gibbons@ucas-edu.net>
2026-02-24 12:00:27 -0700

    partial notes clean up

--- c/notes/strings.c
+++ c/notes/strings.c
@@ -1,17 +1,16 @@
 // Azg strings notes
-#include <studio.h>
+#include <stdio.h>
 #include <strings.h>
 int main(){
-   char subject[] = "Computer Science Principles"
+   char subject[] = "Computer Science Principles";
    char book[50];
    char title "Ms.LaRose";
    int num;
-   printf("%s", subject)
-   num = 10
-   printf("write the title of a book") 
-   fgets(book, sizeof(book), stdin)
-   primtf("\nThe book is %s. 
-   that is a cool book\n", book);
+   printf("%s", subject);
+   num = 10;
+   printf("write the title of a book") ;
+   fgets(book, sizeof(book), stdin);
+   primtf("\nThe book is %s. that is a cool book\n", book);
 
    char name[]= "Vienna";
    last[] = "LaRode";
