69c713565c580a316440aea00c8cf4b3053791ba
ZaleyBear <alice.gibbons@ucas-edu.net>
2026-02-27 11:18:56 -0700

    merging

---
+++ c/practices/time_of_day.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char hour[2]
+    strcat 
+    if (time >= 5){
+        printf("Goodmorning!\n");}
+}else if (time >= 12){
+        printf("Good Afternoon!\n");
+}else if (time >= 16){
+        printf("Good Evening!\n");
+}else if (time >= 20){
+        printf("Goodnight!\n");
+}else {printf("Sorry...! I dot\n");}
