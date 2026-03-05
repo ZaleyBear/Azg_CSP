c23615e92bc373abaf2729978468fdc4c2ac20ad
ZaleyBear <alice.gibbons@ucas-edu.net>
2026-03-05 11:05:01 -0700

    boo

--- c/notes/Loops.c
+++ c/notes/Loops.c
@@ -1,10 +1,41 @@
 //AZG Loops Notes
 
 //While loops
+
+
 #include <stdio.h>
-
+// includes to get a random number
+//Example 1.1 counting to ten
+#include <stdlib.h>
+#include <time.h>
 int main(){
+    int i = 1;
+ while (i <= 10){
+    printf("%d\n", i);
+    i++;
+ }
+ //Example 1.2
+//generate random number
+srand(time(NULL));
+printf("%d\n", rand() % 10); //random between 0 and 10
+printf("%d\n", (rand() % 4) +1);//number between 0 and four, plus one. (number between 1 and five)
+printf("%d\n", rand() % 20);
 
+//Example 2 duck duck goose
+int goose = (rand() % 9)+1;
+int count  = 1;
+while (count < goose){
+    printf("Duck!\n");
+    count++; //Increase by 1
+}
+printf("GOOSE!\n");
 
+//example 3 counting backwards by twos
+int timer = 30;
+while (timer >= 0){
+    printf("%d\n", timer);
+    timer= timer-2;
+}
+printf("Times up!");
     return 0;
 }
--- c/notes/lists.c
+++ c/notes/lists.c
@@ -1,1 +1,26 @@
 //AZG Lists notes
+
+#include <stdio.h>
+// array = list
+int main(){
+//Example 1
+int grades[] = {74, 88, 95, 87, 98};
+printf("%d\n", grades[2]);
+//would print 95
+
+//Example 2
+float sizes[] = {3.57, 24.95, 36.1, 5.99};
+//change an item in my list
+printf("%.2f\n", sizes[0]);
+sizes[0] = 10.45;
+printf("%.2f\n", sizes[0]);
+
+//Example 3 - strings
+char names[][20]={"Atticus", "Sawyer," "Brinkly", "Zee", "Morticia", "Buffy", "Mylo", "Alyiah"}; 
+//first bracket is for number of items in the array, second bracket is for number of letters allowed in each string
+ printf("%s\n", names[5]);
+
+
+
+    return 0;
+}
